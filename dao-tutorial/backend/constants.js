ec927bbc162a8c514a9c9420fa00baf233210915
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2023-02-15 01:46:59 +0530

    dao-tutorial

---
+++ dao-tutorial/backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x311E94c8b84df2D66175ff2fA893d361D42f556E";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
